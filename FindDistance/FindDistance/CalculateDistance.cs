faf5dd46225abe33bc5e806ac5cd1ae20cd3d13f
Drunamefit <yaxa_98@mail.ru>
2018-10-16 20:21:27 +0600

    version 4.0

---
+++ FindDistance/FindDistance/CalculateDistance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDistance
+{
+
+    public class GeoPoint
+    {
+        public double latitudeFirst;
+        public double longitudeFirst;
+
+    }
+
+        public class CalculateDistance : GeoPoint
+        {
+        public CalculateDistance(double latitudeFirst, double longitudeFirst) 
+        {
+            this.latitudeFirst = latitudeFirst;
+            this.longitudeFirst = longitudeFirst;
+        }
+
+        public double Distance(GeoPoint y)
+            {
+                var longitude = inRad(this.longitudeFirst - y.longitudeFirst);
+                var latitude = inRad(this.latitudeFirst - y.latitudeFirst);
+
+                var a = Math.Pow(Math.Sin(latitude / 2), 2) +
+                    Math.Cos(inRad(this.latitudeFirst)) *
+                    Math.Cos(inRad(y.latitudeFirst)) *
+                    Math.Pow(Math.Sin(longitude / 2), 2);
+
+                var c = 2 * Math.Asin(Math.Sqrt(a));
+
+                var earthRadiusInKms = 6371;
+                var distance = earthRadiusInKms * c;
+                distance = Convert.ToInt32(distance);
+                return distance;
+
+
+            }
+            private double inRad(double inRad)
+            {
+                return inRad * (Math.PI / 180);
+            }
+
+
+        }
+    }
+
+    
---
+++ FindDistance/FindDistance/FindDistance.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+namespace FindDistance
+{
+    public class Program 
+    {
+
+        static void Main(string[] args)
+        {
+
+            var latitudeFirst = 0.0;
+            var longitudeFirst = 0.0;
+            var latitudeSecond = 0.0;
+            var longitudeSecond = 0.0;
+
+            do
+            {
+                Console.WriteLine("Введите широту первой точки :");
+                latitudeFirst = Double.Parse(Console.ReadLine());
+                if (latitudeFirst < -90 || latitudeFirst > 90)
+                {
+                    Console.WriteLine("Такой широты не существует!");
+                    longitudeFirst = 0;
+                    continue;
+                }
+                Console.WriteLine("Введите долготу первой точки:");
+                longitudeFirst = Double.Parse(Console.ReadLine());
+                if (longitudeFirst < 0 || longitudeFirst > 180)
+                {
+                    Console.WriteLine("Такой долготы не существует!");
+                    continue;
+                }
+            } while (latitudeFirst < -90 || latitudeFirst > 90 || longitudeFirst < 0 || longitudeFirst > 180);
+            
+            do
+            {
+                Console.WriteLine("Введите широту второй точки :");
+                latitudeSecond = Double.Parse(Console.ReadLine());
+                if (latitudeSecond < -90 || latitudeSecond > 90)
+                {
+                    Console.WriteLine("Такой широты не существует!");
+                    longitudeSecond = 0;
+                    continue;
+                }
+                Console.WriteLine("Введите долготу второй точки");
+                longitudeSecond = Double.Parse(Console.ReadLine());
+                if (longitudeSecond < 0 || longitudeSecond > 180)
+                {
+                    Console.WriteLine("Такой долготы не существует!");
+                    continue;
+
+                }
+            }
+            while (latitudeSecond < -90 || latitudeSecond > 90 || longitudeSecond < 0 || longitudeSecond > 180);
+
+            var x = new CalculateDistance(latitudeFirst, longitudeFirst);
+            var y = new CalculateDistance(latitudeSecond, longitudeSecond);
+            var distance = x.Distance(y);
+            
+            Console.WriteLine("Растояние между точками равно : " + distance + " КМ");
+                Console.ReadKey();
+
+            }
+        }
+    }
+
+
+    
